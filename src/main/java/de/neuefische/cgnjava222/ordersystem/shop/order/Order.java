5faaaecb8753ce4202a3f4bbe7d668b97dfc0d91
Matthias Hiemer <matthias.hiemer@neuefische.de>
2022-08-19 18:10:37 +0200

    Change model from record to class

--- src/main/java/de/neuefische/cgnjava222/ordersystem/shop/order/Order.java
+++ src/main/java/de/neuefische/cgnjava222/ordersystem/shop/order/Order.java
@@ -3,9 +3,48 @@ package de.neuefische.cgnjava222.ordersystem.shop.order;
 import de.neuefische.cgnjava222.ordersystem.shop.product.Product;
 
 import java.util.List;
+import java.util.Objects;
 
-public record Order(
-        int id,
-        List<Product> products
-) {
+public final class Order {
+
+    private final int id;
+    private final List<Product> products;
+
+    public Order(
+            int id,
+            List<Product> products
+    ) {
+        this.id = id;
+        this.products = products;
+    }
+
+    public int id() {
+        return id;
+    }
+
+    public List<Product> products() {
+        return products;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Order) obj;
+        return this.id == that.id &&
+                Objects.equals(this.products, that.products);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, products);
+    }
+
+    @Override
+    public String toString() {
+        return "Order[" +
+                "id=" + id + ", " +
+                "products=" + products + ']';
+    }
+
 }
--- src/main/java/de/neuefische/cgnjava222/ordersystem/shop/product/Product.java
+++ src/main/java/de/neuefische/cgnjava222/ordersystem/shop/product/Product.java
@@ -1,4 +1,44 @@
 package de.neuefische.cgnjava222.ordersystem.shop.product;
 
-public record Product(int id, String name) {
+import java.util.Objects;
+
+public final class Product {
+
+    private final int id;
+    private final String name;
+
+    public Product(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int id() {
+        return id;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Product) obj;
+        return this.id == that.id &&
+                Objects.equals(this.name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Product[" +
+                "id=" + id + ", " +
+                "name=" + name + ']';
+    }
+
 }
